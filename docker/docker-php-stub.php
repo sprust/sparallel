80582fceacaffb9b2166b9ba7e685e616795ddf0
spp28 <sppetrovich@mail.ru>
2025-05-30 15:37:36 +0200

    add user and group support in docker builds, enable pcntl extension, and update docker-compose and related configs

--- docker/docker-php-stub.php
+++ docker/docker-php-stub.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-while (true) {
+pcntl_async_signals(true);
+
+$stop = false;
+
+pcntl_signal(SIGTERM, function () use (&$stop) {
+    $stop = true;
+
+    echo "received SIGTERM. Exit...\n";
+});
+
+while (!$stop) {
     echo 'ping: ' . time() . PHP_EOL;
 
     sleep(10);
